ddf1e297af048c9c1a79c9849db3ed8271c373a1
Raul Dionisio <raul.dionisio@cortex-intelligence.com>
2023-06-02 17:54:01 -0300

    Adicionando padrão strategy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.imposto.TipoImpostoEnum;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+
+        //sem strategy
+        System.out.println(calculadora.calcular(orcamento, TipoImpostoEnum.ISS));
+
+        //com strategy
+        System.out.println(calculadora.calcular(orcamento, new ISS()));
     }
 }
---
+++ src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
@@ -0,0 +1,34 @@
+package br.com.alura.loja.imposto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class CalculadoraDeImpostos {
+
+    //Calcula o valor do imposto para cada orçamento de acordo com a alíquota de imposto
+    public BigDecimal calcular(Orcamento orcamento, TipoImpostoEnum tipoImposto) {
+
+        // Funciona, porém ficará cada vez maior sempre que um novo imposto for criado, pois será necessário
+        // colocar um novo case aqui
+        switch (tipoImposto) {
+            case ICMS:
+                return orcamento.getValor().multiply(new BigDecimal("0.1"));
+            case ISS:
+                return orcamento.getValor().multiply(new BigDecimal("0.6"));
+            default:
+                return BigDecimal.ZERO;
+        }
+    }
+
+    //Para solucionar vamos criar uma classe para cada imposto ao invés de passar o enum
+    public BigDecimal calcular(Orcamento orcamento, Imposto imposto) {
+        return imposto.calcular(orcamento);
+    }
+
+    /*
+    * agora se surgirem novos impostos na aplicação nós não precisamos mais mexer na calculadora,
+    * o código dela vai continuar intacto, vai continuar tendo apenas uma única linha.
+    * E cada classe de imposto vai ter a sua implementação em específica.
+    * Então o código de cada classe também fica pequeno, também fica bem coeso*/
+}
