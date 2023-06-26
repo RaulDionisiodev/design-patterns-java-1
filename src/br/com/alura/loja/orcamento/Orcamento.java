233ec916811da57772f235880571b2506a97982e
Raul Dionisio <raul.dionisio@cortex-intelligence.com>
2023-06-26 18:35:36 -0300

    Adicionando padrão State

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,15 +1,20 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
 
     private BigDecimal valor;
     private  int quantidadeItens;
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
     }
 
     public BigDecimal getValor() {
@@ -22,5 +27,31 @@ public class Orcamento {
 
     public void setQuantidadeItens(int quantidadeItens) {
         this.quantidadeItens = quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra () {
+        BigDecimal valorDoDescontoExtra = situacao.calcularValorDescontoExtra(this);
+
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar() {
+        this.situacao.reporvar(this);
+    }
+
+    public void finalizar() {
+        this.situacao.finalizar(this);
+    }
+
+    public void setSituacao(SituacaoOrcamento orcamento) {
+        this.situacao = situacao;
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
     }
 }
